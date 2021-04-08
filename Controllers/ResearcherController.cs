81f17468abcebde01e015bf997264ebc24a31b16
MitchRogers <mitchellmel@outlook.com>
2021-04-08 11:28:26 -0600

    new controllers, indexes

---
+++ Controllers/ResearcherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BYUEgyptExcavation.Controllers
+{
+    public class ResearcherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
