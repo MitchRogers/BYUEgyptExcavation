edae514879c57e79b82459db06e8891ac4f23ce1
Keetchc <70861267+Keetchc@users.noreply.github.com>
2021-04-09 12:30:52 -0600

    Auth/Author

--- Models/BYUEgyptExcavationsFagelGamousContext.cs
+++ Models/BYUEgyptExcavationsFagelGamousContext.cs
@@ -25,7 +25,7 @@ namespace BYUEgyptExcavation.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=byuegyptexcavations.cojcf6npt8v9.us-west-1.rds.amazonaws.com;Initial Catalog=BYU Egypt Excavations Fag el-Gamous;User ID=admin;Password=Admin321!;MultipleActiveResultSets=true");
             }
         }
