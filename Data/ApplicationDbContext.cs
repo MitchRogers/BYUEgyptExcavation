766959e9b40ad88e9a232b8a791cdce30d4b335f
Keetchc <70861267+Keetchc@users.noreply.github.com>
2021-04-09 15:30:03 -0600

    Burials database changes.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BYUEgyptExcavation.Models;
 
 namespace BYUEgyptExcavation.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
