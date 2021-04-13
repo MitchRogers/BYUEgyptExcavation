239dffe8bed71d3b1f4c8693e7a37b63f82a6b11
Cameron Keetch <keetchc@byu.edu>
2021-04-12 20:27:31 -0600

    Fixed another error.

--- Models/Filters.cs
+++ Models/Filters.cs
@@ -13,20 +13,20 @@ namespace BYUEgyptExcavation.Models
         {
             //declaring the FilterString which is passed in by the Burial Controller, if it is null, then it is automatically sent to "all-"
             FilterString = filterstring ?? "all-all-all-all-all-all";
-            
+
             //spliting the FilterString into an array, split by the "-" character
             string[] filters = FilterString.Split('-');
-            
-            
+
+
             //sets a specific attribute equal to each position of the array
             PreservationIndex = filters[0];
             HeadDirection = filters[1];
             Gender = filters[2];
-            HairColor = filters[3]; 
+            HairColor = filters[3];
             YearFound = filters[4];
             BurialId = filters[5];
 
-            
+
         }
 
         //declaring the attributes of the filter, these are the columns in our database that we want to be able to filter by.
@@ -38,7 +38,7 @@ namespace BYUEgyptExcavation.Models
         public string Gender { get; }
         public string HairColor { get; }
 
-     
+
         //sets the boolean to true if there is a value in the reference after the lamda is not all.
         public bool HasPreservationIndex => PreservationIndex.ToLower() != "all";
 
@@ -47,5 +47,6 @@ namespace BYUEgyptExcavation.Models
         public bool HasYearFound => YearFound.ToLower() != "all";
         public bool HasGender => Gender.ToLower() != "all";
         public bool HasHairColor => HairColor.ToLower() != "all";
+    }
 }
 
